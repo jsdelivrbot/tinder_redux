a558fd5c15e592d3b9bc925ffc2b334225d1e5ac
tim seo <gdurtyfive@gmail.com>
2017-05-15 16:12:35 -0700

    show detail

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The good parts'},
-    { title: 'Harry potter'},
-    { title: 'The Dark Tower'},
-    { title: 'Eloquent Ruby'}
+    { title: 'Javascript: The good parts', pages: 101 },
+    { title: 'Harry potter', pages: 392},
+    { title: 'The Dark Tower', pages: 3},
+    { title: 'Eloquent Ruby', pages: 1}
   ]
 }
