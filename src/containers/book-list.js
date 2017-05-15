bea24e0baed7590a6bdb9ef4386f3e4b8b976a68
tim seo <gdurtyfive@gmail.com>
2017-05-15 16:16:35 -0700

    maintenance

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -8,9 +8,11 @@ class BookList extends Component {
     return this.props.books.map((book) => {
       return (
         <li
-        key={book.title}
-        onClick={() => this.props.selectBook(book)} 
-        className="list-group-item"> {book.title}</li>
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
       )
     })
   }
